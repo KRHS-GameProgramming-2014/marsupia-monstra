eb2b07ff25275b000f557a8b0bb64b997a1426b7
ben <benjamin.newbern@kearsarge.org>
2014-12-05 09:30:27 -0500

    Worked on text for menu

--- Text.py
+++ Text.py
@@ -1,7 +1,36 @@
-class Text():	
-	def __init__(self, pos, baseText = "Score: ", textSize = 12, textColor=(255,255,255), font = None):
+from Tkinter import *
+
+root = Tk()
+
 	
-		def text (self):
-			self.rect.center = pos
 	
-	
+text1 = Text(root, height=20, width=35)
+photo=PhotoImage(file='marsupiamonstra.png')
+text1.insert(END,'\n')
+text1.image_create(END, image=photo)
+
+text1.pack(side=LEFT)
+
+text2 = Text(root, height=20, width=35)
+scroll = Scrollbar(root, command=text2.yview)
+text2.configure(yscrollcommand=scroll.set)
+text2.tag_configure('bold_italics', font=('Arial', 16, 'bold', 'italic'))
+text2.tag_configure('big', font=('Verdana', 20, 'bold'))
+text2.tag_configure('color', foreground='#476042', 
+						font=('Tempus Sans ITC', 16, 'bold'))
+text2.tag_bind('follow', '<1>', lambda e, t=text2: t.insert(END, "Danny Laughlin and Ben Nebwern "))
+text2.insert(END,'\nMarsupia Monstra\n', 'big')
+quote = """
+Play
+
+Settings
+
+Exit
+
+"""
+text2.insert(END, quote, 'color')
+text2.insert(END, 'Created By\n', 'follow')
+text2.pack(side=LEFT)
+scroll.pack(side=RIGHT, fill=Y)
+
+root.mainloop()
