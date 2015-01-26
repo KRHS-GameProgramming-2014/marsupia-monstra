32bd88b8dd0b30960713d5117b4a87ffaf3178d6
unknown <daniel.laughlin@kearsarge.org>
2015-01-26 09:10:59 -0500

    The game works! and i fixed the background of an image

--- MonsterMain.py
+++ MonsterMain.py
@@ -16,6 +16,8 @@ bgRect = bgImage.get_rect()
 startButton = Button([width/2, height-300], 
 				     "Rsc/Buttons/Start Base.png", 
 				     "Rsc/Buttons/Start Clicked.png")
+		
+character = Player([100,100])
 				     
 startCharacter = pygame.image.load("RSC/Screens/Start Screen.png",
 									"RSC/Screens/Start Screen.png")
@@ -44,11 +46,34 @@ while True:
 		screen.blit(startButton.image, startButton.rect)
 		pygame.display.flip()
 		clock.tick(60)
+		
+		
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: sys.exit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_w or event.key == pygame.K_UP:
+					character.go("up")
+				if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+					character.go("right")
+				if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+					character.go("down")
+				if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+					character.go("left")
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_w or event.key == pygame.K_UP:
+					character.go("stop up")
+				if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+					character.go("stop right")
+				if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+					character.go("stop down")
+				if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+					character.go("stop left")
+
 	
-	
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-	bgColor = r,g,b
-	screen.fill(bgColor)
-	pygame.display.flip()
-	clock.tick(60)
+		bgColor = r,g,b
+		screen.fill(bgColor)
+		screen.blit(character.image, character.rect)
+		character.update(width, height)
+		pygame.display.flip()
+		clock.tick(60)
--- Player.py
+++ Player.py
@@ -1,12 +1,13 @@
 import math,pygame
+from Base import *
 
-class Player():
+class Player(Base):
 	def __init__(self, pos):
-		Player.__init__(self, "Rsc/Player/StationaryDown.png", [0,0], pos)
-		self.upImages = [pygame.image.load("Rsc/Player/StationaryDown.png"),
+		Base.__init__(self, "Rsc/Player/StationaryDown.png", [0,0], pos)
+		self.upImages = [pygame.image.load("Rsc/Player/StationaryUp.png"),
 						 pygame.image.load("Rsc/Player/WalkUp1.png"),
 						 pygame.image.load("Rsc/Player/WalkUp2.png")]
-		self.downImages = [pygame.image.load("Rsc/Player/StationaryUp.png"),
+		self.downImages = [pygame.image.load("Rsc/Player/StationaryDown.png"),
 						   pygame.image.load("Rsc/Player/WalkDown1.png"),
 						   pygame.image.load("Rsc/Player/WalkDown2.png")]
 		self.leftImages = [pygame.image.load("Rsc/Player/StationaryLeft.png"),
@@ -15,15 +16,81 @@ class Player():
 		self.rightImages = [pygame.image.load("Rsc/Player/StationaryRight.png"),
 						    pygame.image.load("Rsc/Player/WalkRight1.png"),
 						    pygame.image.load("Rsc/Player/WalkRight2.png")]
-
-
-	def canmove(self, direction):
+		self.facing = "down"
+		self.changed = False
+		self.images = self.upImages
+		self.frame = 0
+		self.maxFrame = len(self.images) - 1
+		self.waitCount = 0
+		self.maxWait = 60*.25
+		self.image = self.images[self.frame]
+		self.rect = self.image.get_rect(center = self.rect.center)
+		self.maxSpeed = 4
+			
+	def update(self, width, height):
+		Base.update(self, width, height)
+		self.animate()
+		self.changed = False
+		
+	def collideWall(self, width, height):
+		if not self.didBounceX:
+			#print "trying to hit Wall"
+			if self.rect.left < 0 or self.rect.right > width:
+				self.speedx = 0
+				self.didBounceX = True
+				#print "hit xWall"
+		if not self.didBounceY:
+			if self.rect.top < 0 or self.rect.bottom > height:
+				self.speedy = 0
+				self.didBounceY = True
+				#print "hit xWall"
+	
+	def animate(self):
+		if self.waitCount < self.maxWait:
+			self.waitCount += 1
+		else:
+			self.waitCount = 0
+			self.changed = True
+			if self.frame < self.maxFrame:
+				self.frame += 1
+			else:
+				self.frame = 0
+		
+		if self.changed:	
+			if self.facing == "up":
+				self.images = self.upImages
+			elif self.facing == "down":
+				self.images = self.downImages
+			elif self.facing == "right":
+				self.images = self.rightImages
+			elif self.facing == "left":
+				self.images = self.leftImages
+			
+			self.image = self.images[self.frame]
+	
+	def go(self, direction):
 		if direction == "up":
-			pos = (int(self.x),int(self.y-1))
+			self.facing = "up"
+			self.changed = True
+			self.speedy = -self.maxSpeed
+		elif direction == "stop up":
+			self.speedy = 0
 		elif direction == "down":
-			pos = (int(self.x),int(self.y+1))
+			self.facing = "down"
+			self.changed = True
+			self.speedy = self.maxSpeed
+		elif direction == "stop down":
+			self.speedy = 0
+			
+		if direction == "right":
+			self.facing = "right"
+			self.changed = True
+			self.speedx = self.maxSpeed
+		elif direction == "stop right":
+			self.speedx = 0
 		elif direction == "left":
-			pos = (int(self.x-1),int(self.y))
-		elif direction == "right":
-			pos = (int(self.x+1),int(self.y))
-		else: return False
+			self.facing = "left"
+			self.changed = True
+			self.speedx = -self.maxSpeed
+		elif direction == "stop left":
+			self.speedx = 0
